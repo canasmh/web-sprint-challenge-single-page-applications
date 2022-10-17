b282a6db4aaecc5b5391d1d5b858cb3a2acef8fa
canasmh <canasmh@yahoo.com>
2022-10-17 19:35:27 -0400

    Create a header component

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+
+    return (
+        <header style={{textAlign: "center"}}>
+            <h1>BloomTeach Eats</h1>
+        </header>
+    )
+}
+
+export default Header;
