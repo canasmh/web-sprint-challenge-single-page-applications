a5763bf0d7a98d76b10eba92df0ca0bb9f93e9d8
canasmh <canasmh@yahoo.com>
2022-10-17 19:42:36 -0400

    Set up home page and pizza form page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from "react";
+import { Route, Switch } from "react-router-dom";
 import Header from "./Header";
+import Home from "./Home";
+import PizzaForm from "./PizzaForm";
 
 const App = () => {
   return (
     <>
       <Header />
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/get-pizza">
+          <PizzaForm />
+        </Route>
+      </Switch>
     </>
   );
 };
---
+++ src/PizzaForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function PizzaForm() {
+    
+    return (
+        <>
+            <h2>This is the Pizza Form page</h2>
+        </>
+    )
+}
+
+export default PizzaForm;
