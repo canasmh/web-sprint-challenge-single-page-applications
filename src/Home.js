98f0facc2669bde0c4f5292dfa21aaae1ae5d315
canasmh <canasmh@yahoo.com>
2022-10-17 19:51:46 -0400

    Use flex box to vertically and horizontally center text in background image

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,11 @@ const backgroundStyle = {
     background: "linear-gradient( rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7) ), url('https://t4.ftcdn.net/jpg/02/75/92/41/360_F_275924184_GP4ACV80EK8NokxlFaUB2qykpXKrroqx.jpg')",
     height: "80vh",
     width: "100vw",
-    color: "#FFFFFF"
+    color: "#FFFFFF",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+
 }
 
 function Home() {
