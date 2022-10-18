df5af51bfbce91750242ded12ffb365008b08155
canasmh <canasmh@yahoo.com>
2022-10-17 20:58:22 -0400

    Begin building pizza form

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,14 @@
 import React from "react";
 
+
+const headerStyle = {
+    color: "#990000"
+}
 function Header() {
 
     return (
         <header style={{textAlign: "center"}}>
-            <h1>BloomTeach Eats</h1>
+            <h1 style={headerStyle}>BloomTeach Eats</h1>
         </header>
     )
 }
--- src/PizzaForm.js
+++ src/PizzaForm.js
@@ -1,11 +1,31 @@
 import React from "react";
 
+
+const styleDiv = {
+    width: "40vw",
+    margin: "15px auto",
+    textAlign: "center",
+    border: "2px solid #990000",
+    borderRadius: "8px"
+}
+
+const styleImg = {
+    width: "100%",
+    border: "1px solid #990000"
+}
+
 function PizzaForm() {
     
     return (
-        <>
-            <h2>This is the Pizza Form page</h2>
-        </>
+        <div style={styleDiv}>
+            <h3 style={{color: "#990000"}}>Build Your Own Pizza</h3>
+            <img src="https://www.dogtownpizza.com/wp-content/uploads/2019/03/pepperoni-pizza-background.jpg" alt="yummy pizza" style={styleImg}/>
+            
+            <form id="pizza-form" style={{textAlign: "left", padding: "8px 16px"}}>
+            <p>Build your own pizza: </p>
+
+            </form>
+        </div>
     )
 }
 
