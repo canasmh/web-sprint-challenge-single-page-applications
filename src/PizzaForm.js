256136fdc1d4cd3b4eaa83a72ad7952a5ba55d5e
canasmh <canasmh@yahoo.com>
2022-10-18 10:36:05 -0400

    Create state to hold form data and errors. Create a function that handles changes on form data

--- src/PizzaForm.js
+++ src/PizzaForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 
 const styleDiv = {
@@ -23,7 +23,24 @@ const inputDiv = {
     padding: "16px"
 }
 
+const initForm = {
+    name: '',
+    size: '',
+    toppings: [],
+    instructions: ''
+
+}
+
 function PizzaForm() {
+
+    const [formData, setFormData] = useState(initForm)
+    const [errors, setErrors] = useState(initForm)
+
+    const handleChange = (event) => {
+        console.log(`name: ${event.target.name}`)
+        console.log(`value: ${event.target.value}`)
+        console.log(`type: ${event.target.type}`)
+    }
     
     return (
         <div style={styleDiv}>
@@ -36,7 +53,13 @@ function PizzaForm() {
                     <p>Required</p>
                 </div>
                 <div style={inputDiv}>
-                    <input type="text" id="name-input" style={{width: "15rem", padding: "12px 4px"}}></input>
+                    <input 
+                        type="text" 
+                        id="name-input" 
+                        style={{width: "15rem", padding: "12px 4px"}} 
+                        name="name" 
+                        onChange={handleChange}
+                    />
                 </div>
                 
                 <div style={labelDiv}>
@@ -44,7 +67,12 @@ function PizzaForm() {
                     <p>Required</p>
                 </div>
                 <div style={inputDiv}>
-                    <select id="size-dropdown" style={{width: "15rem", padding: "12px 4px"}}>
+                    <select 
+                        id="size-dropdown" 
+                        style={{width: "15rem", padding: "12px 4px"}} 
+                        name="size"
+                        onChange={handleChange}
+                    >
                         <option value={null}>Select</option>
                         <option value="s">Small</option>
                         <option value="m">Medium</option>
@@ -59,41 +87,41 @@ function PizzaForm() {
                 </div>
                 <div style={inputDiv}>
                     
-                    <input type="checkbox" id="pepperoni" name="toppings" style={{marginRight: "8px"}} value="pepperoni"></input>
+                    <input type="checkbox" id="pepperoni" name="toppings" style={{marginRight: "8px"}} value="pepperoni" onChange={handleChange}></input>
                     <label htmlFor="pepperoni">Pepperoni</label>
                     <br />
-                    <input type="checkbox" id="sausage" name="toppings" style={{marginRight: "8px"}} value="sausage"></input>
+                    <input type="checkbox" id="sausage" name="toppings" style={{marginRight: "8px"}} value="sausage" onChange={handleChange}></input>
                     <label htmlFor="sausage">Sausage</label>
                     <br />
-                    <input type="checkbox" id="chicken" name="toppings" style={{marginRight: "8px"}} value="chicken"></input>
+                    <input type="checkbox" id="chicken" name="toppings" style={{marginRight: "8px"}} value="chicken" onChange={handleChange}></input>
                     <label htmlFor="chicken">Chicken</label>
                     <br />
-                    <input type="checkbox" id="steak" name="toppings" style={{marginRight: "8px"}} value="steak"></input>
+                    <input type="checkbox" id="steak" name="toppings" style={{marginRight: "8px"}} value="steak" onChange={handleChange}></input>
                     <label htmlFor="steak">Steak</label>
                     <br />
-                    <input type="checkbox" id="olives" name="toppings" style={{marginRight: "8px"}} value="olives"></input>
+                    <input type="checkbox" id="olives" name="toppings" style={{marginRight: "8px"}} value="olives" onChange={handleChange}></input>
                     <label htmlFor="olives">Olives</label>
                     <br />
-                    <input type="checkbox" id="peppers" name="toppings" style={{marginRight: "8px"}} value="peppers"></input>
+                    <input type="checkbox" id="peppers" name="toppings" style={{marginRight: "8px"}} value="peppers" onChange={handleChange}></input>
                     <label htmlFor="peppers">Peppers</label>
                     <br />
-                    <input type="checkbox" id="pineapple" name="toppings" style={{marginRight: "8px"}} value="pineapple"></input>
+                    <input type="checkbox" id="pineapple" name="toppings" style={{marginRight: "8px"}} value="pineapple" onChange={handleChange}></input>
                     <label htmlFor="pineapple">Pineapple</label>
                     <br />
-                    <input type="checkbox" id="ham" name="toppings" style={{marginRight: "8px"}} value="ham"></input>
+                    <input type="checkbox" id="ham" name="toppings" style={{marginRight: "8px"}} value="ham" onChange={handleChange}></input>
                     <label htmlFor="ham">Ham</label>
                     <br />
-                    <input type="checkbox" id="oregano" name="toppings" style={{marginRight: "8px"}} value="oregano"></input>
+                    <input type="checkbox" id="oregano" name="toppings" style={{marginRight: "8px"}} value="oregano" onChange={handleChange}></input>
                     <label htmlFor="oregano">Oregano</label>
                     <br />
-                    <input type="checkbox" id="spinach" name="toppings" style={{marginRight: "8px"}} value="spinach"></input>
+                    <input type="checkbox" id="spinach" name="toppings" style={{marginRight: "8px"}} value="spinach" onChange={handleChange}></input>
                     <label htmlFor="spinach">Spinach</label>
                 </div>
                 <div style={labelDiv}>
                     <label htmlFor="special-text"><h4>Special Instructions</h4></label>
                 </div>
                 <div style={inputDiv}>
-                    <textarea rows="3" cols={50} id="special-text"></textarea>
+                    <textarea rows="3" cols={50} id="special-text" name="instructions" onChange={handleChange}></textarea>
                 </div>
 hjl
             </form>
