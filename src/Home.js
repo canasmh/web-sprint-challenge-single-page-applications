1355aab39b5ff9b459e3ea0143be04805af1383f
canasmh <canasmh@yahoo.com>
2022-10-17 20:40:32 -0400

    Style the get pizza button and also create the link to the pizza route

--- src/Home.js
+++ src/Home.js
@@ -1,20 +1,31 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const backgroundStyle = {
-    background: "linear-gradient( rgba(0, 0, 0, 0.7), rgba(0, 0, 0, 0.7) ), url('https://t4.ftcdn.net/jpg/02/75/92/41/360_F_275924184_GP4ACV80EK8NokxlFaUB2qykpXKrroqx.jpg')",
+    background: "linear-gradient( rgba(0, 0, 0, 0.85), rgba(0, 0, 0, 0.85) ), url('https://www.dogtownpizza.com/wp-content/uploads/2019/03/pepperoni-pizza-background.jpg')",
     height: "80vh",
     width: "100vw",
     color: "#FFFFFF",
     display: "flex",
     justifyContent: "center",
     alignItems: "center"
+}
 
+const buttonStyle = {
+    padding: "12px 24px",
+    fontSize: "1.6rem",
+    backgroundColor: "inherit",
+    color: "#FFFFFF",
+    border: "2px solid #FFFFFF",
+    borderRadius: "16px",
+    margin: "0",
+    cursor: "pointer"
 }
 
 function Home() {
     return (
         <section style={backgroundStyle}>
-            <h2>This is the Home page</h2>
+            <Link to="/pizza"><button id="order-pizza" style={buttonStyle}>Order a Pizza</button></Link>
         </section>
     )
 }
