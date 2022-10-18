eac1e33370981e73ba88f3e93cf9fd4446fdcaf0
canasmh <canasmh@yahoo.com>
2022-10-18 10:35:37 -0400

    Create checkboxes for toppings.

--- src/PizzaForm.js
+++ src/PizzaForm.js
@@ -14,16 +14,88 @@ const styleImg = {
     border: "1px solid #990000"
 }
 
+const labelDiv = {
+    backgroundColor: "#DDDDDD",
+    padding: "12px 8px"
+}
+
+const inputDiv = {
+    padding: "16px"
+}
+
 function PizzaForm() {
     
     return (
         <div style={styleDiv}>
             <h3 style={{color: "#990000"}}>Build Your Own Pizza</h3>
             <img src="https://www.dogtownpizza.com/wp-content/uploads/2019/03/pepperoni-pizza-background.jpg" alt="yummy pizza" style={styleImg}/>
-            
-            <form id="pizza-form" style={{textAlign: "left", padding: "8px 16px"}}>
-            <p>Build your own pizza: </p>
+            <form id="pizza-form" style={{textAlign: "left"}}>
+                <p style={{marginLeft: "8px"}}>Build your own pizza: </p>
+                <div style={labelDiv}>
+                    <label htmlFor="name-input"><h4>Please Enter Your Name</h4></label>
+                    <p>Required</p>
+                </div>
+                <div style={inputDiv}>
+                    <input type="text" id="name-input" style={{width: "15rem", padding: "12px 4px"}}></input>
+                </div>
+                
+                <div style={labelDiv}>
+                    <label htmlFor="size-dropdown"><h4>Choice of Size</h4></label>
+                    <p>Required</p>
+                </div>
+                <div style={inputDiv}>
+                    <select id="size-dropdown" style={{width: "15rem", padding: "12px 4px"}}>
+                        <option value={null}>Select</option>
+                        <option value="s">Small</option>
+                        <option value="m">Medium</option>
+                        <option value="l">Large</option>
+                        <option value="xl">X-Large</option>
+                    </select>
+                </div>
 
+                <div style={labelDiv}>
+                    <h4>Please Select Your Toppings</h4>
+                    <p>Select up to 5</p>
+                </div>
+                <div style={inputDiv}>
+                    
+                    <input type="checkbox" id="pepperoni" name="toppings" style={{marginRight: "8px"}} value="pepperoni"></input>
+                    <label htmlFor="pepperoni">Pepperoni</label>
+                    <br />
+                    <input type="checkbox" id="sausage" name="toppings" style={{marginRight: "8px"}} value="sausage"></input>
+                    <label htmlFor="sausage">Sausage</label>
+                    <br />
+                    <input type="checkbox" id="chicken" name="toppings" style={{marginRight: "8px"}} value="chicken"></input>
+                    <label htmlFor="chicken">Chicken</label>
+                    <br />
+                    <input type="checkbox" id="steak" name="toppings" style={{marginRight: "8px"}} value="steak"></input>
+                    <label htmlFor="steak">Steak</label>
+                    <br />
+                    <input type="checkbox" id="olives" name="toppings" style={{marginRight: "8px"}} value="olives"></input>
+                    <label htmlFor="olives">Olives</label>
+                    <br />
+                    <input type="checkbox" id="peppers" name="toppings" style={{marginRight: "8px"}} value="peppers"></input>
+                    <label htmlFor="peppers">Peppers</label>
+                    <br />
+                    <input type="checkbox" id="pineapple" name="toppings" style={{marginRight: "8px"}} value="pineapple"></input>
+                    <label htmlFor="pineapple">Pineapple</label>
+                    <br />
+                    <input type="checkbox" id="ham" name="toppings" style={{marginRight: "8px"}} value="ham"></input>
+                    <label htmlFor="ham">Ham</label>
+                    <br />
+                    <input type="checkbox" id="oregano" name="toppings" style={{marginRight: "8px"}} value="oregano"></input>
+                    <label htmlFor="oregano">Oregano</label>
+                    <br />
+                    <input type="checkbox" id="spinach" name="toppings" style={{marginRight: "8px"}} value="spinach"></input>
+                    <label htmlFor="spinach">Spinach</label>
+                </div>
+                <div style={labelDiv}>
+                    <label htmlFor="special-text"><h4>Special Instructions</h4></label>
+                </div>
+                <div style={inputDiv}>
+                    <textarea rows="3" cols={50} id="special-text"></textarea>
+                </div>
+hjl
             </form>
         </div>
     )
